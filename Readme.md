b2f1f3fdae62c9c0eb89c8f7f33a33c1e84e578a
JanSchuermannPH <jsbaguio.ph@gmail.com>
2016-04-20 23:19:54 +0800

    Init

---
+++ Readme.md
@@ -0,0 +1,51 @@
+# AngularJS VisualBoolean Filter
+
+Display boolean values with the help of icons
+
+## Usage
+
+####Apply the filter to a value and see the magic happen!
+```
+<span ng-bind-html="myValue | visualBoolean"></span>
+Result:
+<span><i class="fa fa-lg fa-check"></i></span>
+```
+####Change the icon size
+```javascript
+app.config(function(visualBooleanFilterConfigProvider){
+    visualBooleanFilterConfigProvider.changeIconSize(2);
+})
+```
+```
+<span ng-bind-html="myValue | visualBoolean"></span>
+Result:
+<span><i class="fa fa-2x fa-check"></i></span>
+```
+*Available Icon Sizes are 1 (fa-lg), 2 (fa-2x),3 (fa-3x), 4 (fa-4x) and 5 (fa-5x)*
+
+
+####Add additional classes to the icon:
+```
+app.config(function(visualBooleanFilterConfigProvider){
+    visualBooleanFilterConfigProvider.addIconClass('yourClassName');
+})
+```
+####Add multiple classes at once:
+```
+app.config(function(visualBooleanFilterConfigProvider){
+    visualBooleanFilterConfigProvider.addIconClass(['class1','class2']);
+})
+```
+
+####Remove classes from the icon:
+```
+app.config(function(visualBooleanFilterConfigProvider){
+    visualBooleanFilterConfigProvider.removeIconClass('yourClassName');
+})
+```
+####Remove multiple classes at once:
+```
+app.config(function(visualBooleanFilterConfigProvider){
+    visualBooleanFilterConfigProvider.removeIconClass(['class1','class2']);
+})
+```
--- visualBooleanFilter.js
+++ visualBooleanFilter.js
@@ -1,10 +1,86 @@
 angular.module('visualBooleanFilter',[])
     .provider('visualBooleanFilterConfig',function(){
-        this.defaultIcons = {
-            'true':'',
-            'false':''
+        var $ = this;
+        $.defaultValues = {"true":true,"false":false};
+        $.changeDefaultValue = function(prop,value){$.defaultValues[prop] = value;};
+        $.getDefaultValues = function(){return $.defaultValues};
+
+        $.defaultIcons = {};
+        $.defaultIcons[this.defaultValues.true] = 'fa-check';
+        $.defaultIcons[this.defaultValues.false] = 'fa-times';
+        $.icon = function(value){return $.defaultIcons[value];};
+        $.changeIcon = function(name,icon){$.defaultIcons[name] = icon};
+
+        $.iconClasses = ['fa','fa-lg'];
+        $.addIconClass = function(className){
+            if(Array.isArray(className))
+            {
+                angular.forEach(className,function(v){
+                    if($.iconClasses.indexOf(v) < 0)
+                    {
+                        $.iconClasses.push(v);
+                    }
+                });
+            }
+            else
+            {
+                if($.iconClasses.indexOf(className) < 0)
+                {
+                    $.iconClasses.push(className);
+                }
+            }
         };
-        this.setIcon = function(value,icon){};
-        this.$get = function(){return this;};
+        $.removeIconClass = function(className)
+        {
+            if(Array.isArray(className))
+            {
+                var indexOfClassName = $.iconClasses.indexOf(className);
+                if(indexOfClassName >= 0)
+                {
+                    $.iconClasses.splice(indexOfClassName,1);
+                }
+            }
+            else
+            {
+                angular.forEach(className,function(v){
+                    var indexOfClassName = $.iconClasses.indexOf(v);
+                    if(indexOfClassName >= 0)
+                    {
+                        $.iconClasses.splice(indexOfClassName,1);
+                    }
+                });
+            }
+        };
+        $.changeIconSize = function(newSize){
+            var iconSizes = ['fa-lg','fa-2x','fa-3x','fa-4x','fa-5x'];
+            if(typeof newSize === "number")
+            {
+                angular.forEach(iconSizes,function(v){$.removeIconClass(v)});
+                if(newSize === 1)
+                {
+                    $.addIconClass('fa-lg');
+                }
+                else
+                {
+                    $.addIconClass('fa-' + newSize + 'x');
+                }
+            }
+        };
+        $.getIconClasses = function(){return $.iconClasses};
+
+        $.$get = function(){return this;};
     })
-    .filter('visualBoolean',function(){});
+    .filter('visualBoolean',['$sce','visualBooleanFilterConfig',function($sce,visualBooleanFilterConfig){
+        return function(input){
+            var iconClasses = visualBooleanFilterConfig.getIconClasses();
+            var icon = angular.element('<i>');
+            angular.forEach(iconClasses,function(v){
+                icon.addClass(v);
+            });
+            icon.addClass(visualBooleanFilterConfig.icon(
+                input === visualBooleanFilterConfig.defaultValues.true ?
+                    visualBooleanFilterConfig.defaultValues.true :
+                    visualBooleanFilterConfig.defaultValues.false));
+            return $sce.trustAsHtml(icon.wrap('<span></span>').parent().html());
+        }
+    }]);
